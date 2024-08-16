c19eec0f78ca69e9f1648067d8a440d97eee666a
Nathan Tonini <tonininathan8@gmail.com>
2024-08-16 22:01:49 +0000

    fazendo algumas correçõesedeixando o código mais legível

--- index.py
+++ index.py
@@ -2,7 +2,8 @@ import os
 
 def processar_resposta(resposta, nome):
     if resposta == '1':
-        print(f'{os.linesep}{nome} Vale demais aprender a linguagem python, pois comm ela você consegue construir muitas coisas interessntes')
+        print(f'{os.linesep}{nome} Vale demais aprender a linguagem python, pois com ela você consegue construir muitas coisas interessntes')
+
     if resposta == '2':
         print(f''' 
         Alguns projetos que são possíveis de se fazer usando a linguagem python:
@@ -15,8 +16,9 @@ def processar_resposta(resposta, nome):
         7 - jogos
         8 - conversor de moedas
         ''')
+
     if resposta == '3':
-         print(f'{os.linesep}{nome} Você pode simcomeçar sua  jornada  no mundo da  programação usando a  linguagem python, pois ela é uma linguagem siplesdese  entender e, por isso, perfeita para quem está começando.')
+         print(f'{os.linesep}{nome} Você pode sim começar sua jornada  no mundo da  programação usando a  linguagem python, pois ela é uma linguagem siplesdese  entender e, por isso, perfeita para quem está começando.')
        
 
 def start():
@@ -35,7 +37,7 @@ def start():
 
 
     #processar a resposta
-    processa_resposta(resposta, nome)
+    processar_resposta(resposta, nome)
 
 if __name__ == '__main__':
     start()
