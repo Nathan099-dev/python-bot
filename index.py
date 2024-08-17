502cba45b3629fc4fb93a022325d3c60bf5b8ae5
Nathan099-dev <tonininathan8@gmail.com>
2024-08-17 10:36:46 -0300

    fazendo modificações

--- index.py
+++ index.py
@@ -1,31 +1,37 @@
 import os
 import colorama
+import sys
 from colorama import init, Fore
 
 init()
 RED = Fore.RED
 
 def processar_resposta(resposta, nome):
-    if resposta == '1':
-        print(f'{os.linesep}{nome} Vale demais aprender a linguagem python, pois com ela você consegue construir muitas coisas interessntes')
+    try:
+        if resposta == '1':
+            print(f'{os.linesep}{nome} Vale demais aprender a linguagem python, pois com ela você consegue construir muitas coisas interessntes')
 
-    if resposta == '2':
-        print(f''' 
-        Alguns projetos que são possíveis de se fazer usando a linguagem python:
-        1 - Chatbots
-        2 - Automação de tarefas
-        3 - Dashboards interativos e completos
-        4 - Ferramentas focadas em segurança da informação
-        5 - Ferrametas de análise de dados 
-        6 - calculadora
-        7 - jogos
-        8 - conversor de moedas
-        ''')
+        if resposta == '2':
+            print(f''' 
+                 Alguns projetos que são possíveis de se fazer usando a linguagem python:
+                 1 - Chatbots
+                 2 - Automação de tarefas
+                 3 - Dashboards interativos e completos
+                 4 - Ferramentas focadas em segurança da informação
+                 5 - Ferrametas de análise de dados 
+                 6 - calculadora
+                 7 - jogos
+                 8 - conversor de moedas
+                 ''')
 
-    if resposta == '3':
-         print(f'{os.linesep}{nome} Você pode sim começar sua jornada  no mundo da  programação usando a  linguagem python, pois ela é uma linguagem simples de se entender e, por isso, perfeita para quem está começando.')
-    else:
-        print(f'{RED} Coloque uma as opções acima')
+        if resposta == '3':
+             print(f'{os.linesep}{nome} Você pode sim começar sua jornada  no mundo da  programação usando a  linguagem python, pois ela é uma linguagem simples de se entender e, por isso, perfeita para quem está começando.')
+        else:
+            print(f'{RED} Escolha uma das opções acima')
+    except KeyboardInterrupt:
+        print('Programa interrompido')
+        sys.exit()
+
 
 def start():
     #apresentar o bot
